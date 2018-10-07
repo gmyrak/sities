d574b9b6427bee08a74abb15a9e280ddae1afb79
Dmitry <gmyrak@gmail.com>
2018-10-07 23:28:26 +0300

    Add database

--- game.py
+++ game.py
@@ -2,7 +2,8 @@ f = open('base/city.csv', mode='r', encoding='cp1251')
 
 
 for line in f:
-    print(line)
+    city_id, country_id, region_id, name = (s.strip('"') for s in line.strip().split(';'))
+    print(city_id, country_id, region_id, name)
 
 
 
