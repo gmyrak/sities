75a25e0c6ce064df2e53881ae5de749a4f42123f
Dmitry <gmyrak@gmail.com>
2018-10-08 18:30:57 +0300

    Start implementation

--- game.py
+++ game.py
@@ -1,10 +1,51 @@
+import re
+
+city = {}
+region = {}
+country = {}
+
+class city_info:
+    pass
+
+
+def norma(name):
+    name = re.sub(r'\(.+\)', '', name)
+    name = re.sub(r'\W', '', name)
+    return name.upper()
+
+
 f = open('base/city.csv', mode='r', encoding='cp1251')
-
-
 for line in f:
     city_id, country_id, region_id, name = (s.strip('"') for s in line.strip().split(';'))
-    print(city_id, country_id, region_id, name)
 
+    c = city_info()
+    c.name = name
+    c.city_id = city_id
+    c.region_id = region_id
+    c.country_id = country_id
 
+    key_name = norma(name)
+    city[key_name] = city.get(key_name, ()) + (c,)
 
 f.close()
+
+f = open('base/region.csv', mode='r', encoding='cp1251')
+for line in f:
+    region_id, country_id, city_id, name = (s.strip('"') for s in line.strip().split(';'))
+    region[region_id] = name
+f.close()
+
+f = open('base/country.csv', mode='r', encoding='cp1251')
+for line in f:
+    country_id, city_id, name = (s.strip('"') for s in line.strip().split(';'))
+    country[country_id] = name
+f.close()
+
+
+if __name__ == '__main__':
+
+    for city_name in sorted(city.keys()):
+        for c in city[city_name]:
+            print('{} | {}; {}; {}'.format(city_name, c.name, region[c.region_id], country[c.country_id]))
+            #print('{} '.format(c.name))
+
--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import tkinter as ui
+import game
 
 root = ui.Tk()
 root.geometry('800x600')
 root.title('Города')
-root.resizable(False, False)
+#root.resizable(False, False)
+
+control_panel = ui.Frame(root)
+control_panel.place(width=200, relheight=True)
+
+control_panel.configure(bg = 'red')
 
 
 
