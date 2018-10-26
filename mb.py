5992afce2a33f5022413e80ab1218cd667332bea
Dmitry <gmyrak@gmail.com>
2018-10-26 18:00:56 +0300

    Фрактал

--- mb.py
+++ mb.py
@@ -1,15 +1,20 @@
-class Control():
-    x = 1
-    y = 2
+class Control:
+    def __init__(self):
+        self.x = 4
+        self.y = 2
+        self.R = self.r2()
+
+    def r2(self):
+        return self.x**2 + self.y**2
 
 
 
 ctr1 = Control()
-ctr1.x = 10
+#ctr1.x = 10
 
-print(ctr1.x, ctr1.y)
+print(ctr1.x, ctr1.y, ctr1.R)
 
 ctr2 = Control()
-print(ctr2.x, ctr2.y)
+print(ctr2.x, ctr2.y, ctr2.r2())
 
 
--- mb2.py
+++ mb2.py
@@ -3,156 +3,113 @@ from time import time
 
 
 I_MAX, J_MAX = 800, 600
+ITER_LIMIT = 100
 POINT_R = 3
-
-
-def mandelbrot(c):
-    z = c
-    i = 0
-    while i < iter_limit and abs(z) < 2:
-        z = z**2 + c
-        i += 1
-    return i
-
-
-def size_one(s):
-    return 200 * 10**s
-
-
-class Fractal():
-    i0, j0 = 3 * I_MAX / 5, J_MAX / 2
-    iter_limit = 100
-    scale = 0
-    s1 = size_one(scale)
-    i_point, j_point = X / 2, Y / 2
-    pointer = 0
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-iter_limit = 100
-scale = 0
-
-
-
-s1 = size_one(scale)
-i0, j0 = 3 * I_MAX / 5, J_MAX / 2
-
-
-pointer = 0
-
-cx, cy = I_MAX / 2, J_MAX / 2
-
-def color(n):
-    tone = int(0xFFF - 0xFFF * n / iter_limit)
-    return '#{:03x}{:03x}{:03x}'.format(tone, tone, tone)
-#    if n==MAX_ITER:
-#        return '#000'
-#    else:
-#        return '#fff'
-
-
-def dec_x(i):
-    return (i - i0) / s1
-
-
-def dec_y(j):
-    return (j0 - j) / s1
 
 
 root = Tk()
 root.title('Set of Malderbrot')
 root.resizable(0, 0)
-cn = Canvas(root, width=I_MAX, height=J_MAX, bg='white')
-cn.pack()
-
-
-img = PhotoImage(width=I_MAX, height=J_MAX)
-
-
+CNVS = Canvas(root, width=I_MAX, height=J_MAX, bg='white')
+CNVS.pack()
+PNL = Frame(height=50)
+PNL.pack(fill='x')
 
 
 
+class Fractal():
+    def __init__(self):
+        self.i0, self.j0 = 3 * I_MAX / 5, J_MAX / 2
+        self.iter_limit = ITER_LIMIT
+        self.scale = 0
+        self.s1 = self.size_one(self.scale)
+        self.i_point, self.j_point = I_MAX / 2, J_MAX / 2
+        self.pointer = 0
+        self.img = PhotoImage(width=I_MAX, height=J_MAX)
+        self.draw()
 
-def draw():
-    t0 = time()
-    cn.delete('all')
-    inf = cn.create_text(I_MAX / 2, J_MAX / 2, text='', anchor='center')
-    cn.update()
-    img.blank()
-    per = 0
-    for i in range(I_MAX):
-        per2 = 100 * i // I_MAX
-        if per2 != per:
-            per = per2
-            cn.itemconfig(inf, text='Processing: {:3}%'.format(per), )
-            cn.update()
-        for j in range(J_MAX):
-            c = complex(dec_x(i), dec_y(j))
-            img.put( color(mandelbrot(c)) , to=(i, j))
-    cn.create_image(0, 0, anchor=NW, image=img)
-    cn.create_line(0, j0, I_MAX, j0, arrow='last')
-    cn.create_line(i0, J_MAX, i0, 0, arrow='last')
-    cn.delete(inf)
-    print('Draw fractal {}x{} ({} iter): {:2f} c'.format(I_MAX, J_MAX, iter_limit, time() - t0))
+    def size_one(self, scale):
+        return 200 * 10**scale
+
+    def dec_x(self, i):
+        return (i - self.i0) / self.s1
+
+    def dec_y(self, j):
+        return (self.j0 - j) / self.s1
+
+    def mandelbrot(self, c):
+        z = c
+        i = 0
+        while i < self.iter_limit and abs(z) < 2:
+            z = z ** 2 + c
+            i += 1
+        return i
+
+    def color(self, n):
+        tone = int(0xFFF - 0xFFF * n / self.iter_limit)
+        return '#{:03x}{:03x}{:03x}'.format(tone, tone, tone)
+
+    def draw(self):
+        t0 = time()
+        CNVS.delete('all')
+        inf = CNVS.create_text(I_MAX / 2, J_MAX / 2, text='', anchor='center')
+        CNVS.update()
+        self.img.blank()
+        per = 0
+        for i in range(I_MAX):
+            per2 = 100 * i // I_MAX
+            if per2 != per:
+                per = per2
+                CNVS.itemconfig(inf, text='Processing: {:3}%'.format(per), )
+                CNVS.update()
+            for j in range(J_MAX):
+                c = complex(self.dec_x(i), self.dec_y(j))
+                self.img.put( self.color(self.mandelbrot(c)) , to=(i, j))
+        CNVS.create_image(0, 0, anchor=NW, image=self.img)
+        CNVS.create_line(0, self.j0, I_MAX, self.j0, arrow='last')
+        CNVS.create_line(self.i0, J_MAX, self.i0, 0, arrow='last')
+        CNVS.delete(inf)
+        print('Draw fractal {}x{} ({} iter): {:2f} c'.format(I_MAX, J_MAX, self.iter_limit, time() - t0))
+
+    def set_pointer(self, i, j):
+        if self.pointer > 0:
+            CNVS.delete(self.pointer)
+        self.pointer = CNVS.create_oval(i - POINT_R, j - POINT_R, i + POINT_R, j + POINT_R, fill='yellow', outline='red', tag='p')
+        self.i_point, self.j_point = i, j
+
+    def recalculate(self, new_scale, iter_limit):
+        s2 = self.size_one(new_scale)
+        self.i0 = I_MAX/2 - s2 * (self.i_point - self.i0)/self.s1
+        self.j0 = J_MAX/2 + s2 * (self.j0 - self.j_point)/self.s1
+        self.s1 = s2
+        self.iter_limit = iter_limit
+        self.i_point, self.j_point = I_MAX/2, J_MAX/2
+        self.draw()
 
 
-draw()
 
 
-pn = Frame(height=50)
-pn.pack(fill='x')
+F = Fractal()
 
 
-def cn_click(event):
-    i, j = event.x, event.y
-    global pointer
-    if pointer > 0:
-        cn.delete(pointer)
-    pointer = cn.create_oval(i - POINT_R, j - POINT_R, i + POINT_R, j + POINT_R, fill='yellow', outline='red', tag='p')
-    print(i, j)
-    global cx, cy
-    cx, cy = i, j
+CNVS.bind('<Button-1>', lambda e : F.set_pointer(e.x, e.y))
+
+ent_scale = Entry(PNL, width=8)
+ent_iter = Entry(PNL, width=8)
 
 
-cn.bind('<Button-1>', cn_click)
-
-ent_scale = Entry(pn, width=8)
-ent_iter = Entry(pn, width=8)
-
-def action():
-    global s1, i0, j0, iter_limit, scale
-    s2 = size_one(float(ent_scale.get()))
-    i0 = I_MAX / 2 - s2 * (cx - i0) / s1
-    j0 = J_MAX / 2 + s2 * (j0 - cy) / s1
-    s1 = s2
-    iter_limit = int(ent_iter.get())
-    draw()
-
-
-act = Button(pn, text='Redraw', command=action)
+act = Button(PNL, text='Redraw', command= lambda : F.recalculate(float(ent_scale.get()), int(ent_iter.get())))
 act.pack(side='left')
 
-Label(pn, text='  Scale:').pack(side='left')
+Label(PNL, text='  Scale:').pack(side='left')
 
 
 ent_scale.pack(side='left')
-ent_scale.insert(0, str(scale))
+ent_scale.insert(0, str(F.scale))
 
-Label(pn, text='  Iter: ').pack(side='left')
+Label(PNL, text='  Iter: ').pack(side='left')
 ent_iter.pack(side='left')
-ent_iter.insert(0, str(iter_limit))
+ent_iter.insert(0, str(F.iter_limit))
 
 
 root.mainloop()
